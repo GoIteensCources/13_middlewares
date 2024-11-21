356b745b8d3a7fbcc15ace037121fbce6e33d4d3
Walentin <valoliinykov.work@gmail.com>
2024-11-21 20:41:54 +0200

    midl

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import asyncio
+import time
+from datetime import datetime
+
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Body
 from fastapi.responses import RedirectResponse
 from routers import route
 
 app = FastAPI(description="Learning Middlewares",
               version="0.1")
+
+
+@app.middleware("http")
+async def middleware_root(request: Request, call_next):
+
+    if request.url.path.startswith("/users"):
+        response = await call_next(request)
+        response.headers["Some"] = "Ist users"
+        return response
+
+    print(f"[{datetime.now()}] -- ({request.method}) path: {request.url.hostname}{request.url.path}")
+
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 @app.get("/", include_in_schema=False)
@@ -15,10 +35,11 @@ async def root():
 
 @app.get("/hello")
 async def hello_rout(user: str = "Anonimus"):
+    await asyncio.sleep(0.2)
     return f"Hello, {user}!"
 
 
-app.include_router(route, prefix="/movies", tags=["movie"])
+app.include_router(route, prefix="/users", tags=["users"])
 
 if __name__ == "__main__":
     uvicorn.run(f"{__name__}:app", reload=True)
--- routers.py
+++ routers.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException
 
+
 route = APIRouter()
 
 
 @route.post("/")
-async def create_registration():
+async def create_user():
     ...
 
 
 @route.get("/read/all/")
-async def all_items():
-    ...
+async def all_users():
+    return {"User": "Anonimus"}
